a0b2316136084a052e3a0a84d2782471d686ac9e
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2022-02-26 16:22:37 -0300

    Add initial files

---
+++ src/main/java/br/edu/insper/desagil/aula4/aquecimento/Nave.java
@@ -0,0 +1,22 @@
+// Não é necessário modificar este arquivo.
+// Ele é apenas para ilustrar o aquecimento.
+
+package br.edu.insper.desagil.aula4.aquecimento;
+
+public class Nave {
+	private int x;
+	private int y;
+
+	public Nave(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+}
